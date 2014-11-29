f837447d5d4d30f5261e04cd349485cbccc938eb
Richa Avasthi <ravasthi@gmail.com>
2014-11-29 15:10:47 -0800

    Get sprockets working:
    * Install guard-sprockets
    * Create two master JS manifests: application-header and application. Each will be compiled and minified. Include it in each page.

---
+++ scripts/application.js
@@ -0,0 +1,7 @@
+//= require vendor/jquery
+//= require vendor/jquery-ui.widget
+//= require vendor/fiftyfive-utils/feature-detect
+//= require vendor/fiftyfive-utils/jquery.ui.forminputplaceholdertext
+//= require common
+//= require login
+
