76474ba0a2bf2cf1c9644bb7d595d797cb67a1f3
Richa Avasthi <ravasthi@gmail.com>
2017-09-19 18:02:01 -0700

    WIP for switching out build system
    This time:
    
    * Create source/distribution directories and move files around
    * Basic package.json, webpack.config.js
    * Delete all ruby build files
    
    Next up:
    
    * Update styles to no longer use compass, sassy-buttons
    * Update JS to no longer use sprockets

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'application': './source/scripts/application.js',
+        'application-header': './source/scripts/application-header.js',
+        'styles': './source/styles/application.scss',
+        'ie': './source/styles/ie.scss',
+        'print': './source/styles/print.scss'
+    },
+    output: {
+        filename: 'scripts/[name].js',
+        path: path.resolve(__dirname, 'distribution'),
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                // include: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader' },
+                        { loader: 'postcss-loader' },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                includePaths: [ 'node_modules' ]
+                            }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            ".js",
+            ".json",
+            ".css",
+            ".scss"
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([
+            'distribution/scripts/*.*',
+            'distribution/styles/*.*'
+        ]),
+        new ExtractTextPlugin({
+            filename: 'styles/[name].css',
+            allChunks: true,
+        }),
+        new BrowserSyncPlugin({
+            files: [
+                '*.html',
+                'images/**/*',
+                'distribution/**/*.js',
+                'distribution/**/*.css'
+            ],
+            host: 'localhost',
+            port: 4649,
+            server: {
+                baseDir: ['.']
+            }
+        })
+    ]
+};
