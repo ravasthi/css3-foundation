f8db4d51dcbcc26d49e8e6c9efe9f8d251cddba9
Richa Avasthi <ravasthi@gmail.com>
2020-03-06 04:55:53 -0800

    Remove ios zoom workaround—the bug has been fixed.

--- source/scripts/common.js
+++ source/scripts/common.js
@@ -9,30 +9,7 @@ import $ from 'jquery/dist/jquery';
 // Establish namespace. Change to match the name of your application.
 const Css3Foundation = window.Css3Foundation ? window.Css3Foundation : {};
 
-Css3Foundation.util = {
-  /*
-   ** Workaround for MobileSafari zoom bug after orientation change.
-   ** From: http://adactio.com/journal/4470/
-   */
-  iosZoomWorkaround: () => {
-    const ua = navigator.userAgent;
-    if (ua.match(/iPhone/) || ua.match(/iPad/)) {
-      const viewportmeta = $('meta[name="viewport"]');
-      if (viewportmeta.length > 0) {
-        viewportmeta.attr(
-          'content',
-          'width=device-width, minimum-scale=1.0, maximum-scale=1.0'
-        );
-        $('body').bind('gesturestart', () => {
-          viewportmeta.attr(
-            'content',
-            'width=device-width, minimum-scale=0.25, maximum-scale=1.6'
-          );
-        });
-      }
-    }
-  },
-};
+Css3Foundation.util = {};
 
 $.extend(Css3Foundation, {
   /*
@@ -41,8 +18,6 @@ $.extend(Css3Foundation, {
    ** JS namespace.
    */
   commonInit: () => {
-    Css3Foundation.util.iosZoomWorkaround();
-
     // Other stuff to do as soon as the DOM is ready
   },
 });
